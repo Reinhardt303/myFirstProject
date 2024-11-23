e9c78b7535c40b042b6cbc99085ed0d805409089
Thomas Rains <thomasrains01@gmail.com>
2024-11-22 18:20:56 -0800

    top images and center all details now render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 const characterElement = document.getElementById('character-selection')
-
+const nameElement = document.getElementById('name-display')
+const worldElement = document.getElementById('world-display')
+const imgDetailElement = document.querySelector('.detail-image')
 
 fetch('http://localhost:3000/nintendoCharacters')
 .then(response => response.json())
 .then(characters => {
-  const h1NameElement = document.createElement('h1')
-  h1NameElement.textContent = character.character
+/*   const h1NameElement = document.createElement('h1')
+  h1NameElement.textContent = character.character */
   characters.forEach( character => {
     renderTop(character)
+    renderCenter(character)
   })      //so far so good
 })
 
@@ -16,6 +19,15 @@ const renderTop = (character) => {
   const imgElement = document.createElement('img')
   imgElement.src = character.image
   characterElement.appendChild(imgElement)
+  imgElement.addEventListener('click', () => {
+    renderCenter(character)
+  } )
+}
+
+const renderCenter = (characters) => {
+  nameElement.innerText = characters.character
+  worldElement.innerText = characters.gameSeries
+  imgDetailElement.src = characters.image
 }
 
 
