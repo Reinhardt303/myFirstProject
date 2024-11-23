2abb6a02406ff436c191f3f47587626d3d12ebe0
Thomas Rains <thomasrains01@gmail.com>
2024-11-22 17:49:39 -0800

    5th Top images render, working on card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
+const characterElement = document.getElementById('character-selection')
+
 
 fetch('http://localhost:3000/nintendoCharacters')
 .then(response => response.json())
 .then(characters => {
-console.log(characters)       //so far so good
-  })
+  const h1NameElement = document.createElement('h1')
+  h1NameElement.textContent = character.character
+  characters.forEach( character => {
+    renderTop(character)
+  })      //so far so good
+})
+
+const renderTop = (character) => {
+  const imgElement = document.createElement('img')
+  imgElement.src = character.image
+  characterElement.appendChild(imgElement)
+}
+
+
+
+
+
+
 })
 
 
